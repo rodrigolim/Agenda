1eaf06feee52dbc52aa1863eba7b82df5c0b80a2
Rodrigo CUNHA <rodrigo.cunha@veolia.com>
2023-09-17 18:29:38 -0300

    Update Twilio

--- src/routes/twilio.js
+++ src/routes/twilio.js
@@ -14,43 +14,126 @@ const client = new twilio(accountSid, authToken);
 
 // Endpoint para receber mensagens do WhatsApp
 router.post('/twilio/webhook', (req, res) => {
-
+    let responseMessage = '';
     console.log(req.body);
 
+    const objeto1 = Singleton.getInstance();    
+
+
     const { Body, From, Author } = req.body
+
+
+    objeto1.mensagem = objeto1.mensagem+' - '+Body;
+    objeto1.imprimirMensagem();
+
+
 
     const senderPhoneNumber = (typeof From === 'undefined')  ? Author : From
   
     const text = Body.toLowerCase().replace(/[^\w\s]/gi, "").replace(/[\d]/gi, "").trim();
   
-    const saudacaoRegex = /(ola|bom|dia|oi)/i; // O "i" torna a regex insensível a maiúsculas/minúsculas
+    const saudacaoRegex = /(ola|olá|bom|dia|oi)/i; // O "i" torna a regex insensível a maiúsculas/minúsculas
     const agendamentoRegex = /(marcar|agendamento|agendar)/i; 
     const cancelarRegex = /(cancelar|excluir|deletar)/i; 
+
+    if (saudacaoRegex.test(text)) {
+      objeto1.estado = Estados.SAUDACAO;
+    } else if (agendamentoRegex.test(text)) {
+      objeto1.estado = Estados.MARCAR;
+    } else if (cancelarRegex.test(text)) {
+      objeto1.estado = Estados.CANCELAR;
+    } else if (objeto1.estado === Estados.NOME) {
+      objeto1._nome = text;
+      objeto1.estado = Estados.DATA;
+    } else if (objeto1.estado === Estados.DATA) {
+      objeto1._nome = text;
+      objeto1.estado = Estados.HORA;
+    }
+
+    objeto1.imprimirMensagem();
+
 
 
     // Lógica do chatbot: responder à pergunta "Olá" com uma saudação
     //if (text.match(['ola','bom','dia'])) {
-    if (saudacaoRegex.test(text)) {
-      let responseMessage = 'Olá, tudo bem? Sou o assistente virtual. Como posso ajudar você hoje? \n'+
-                            'Temos as Seguintes opções: \n'+
-                            ' - Marcar horário \n'+
-                            ' - Cancelar horário \n'+
-                            ' - Histórico de horário';
-      sendWhatsAppMessage(senderPhoneNumber, responseMessage);
-    } else  if (agendamentoRegex.test(text)) {
-      let responseMessage = 'Muito bem, me informe seu nome por favor.';
-      sendWhatsAppMessage(senderPhoneNumber, responseMessage);
-    } else  if (cancelarRegex.test(text)) {
-      let responseMessage = 'So um minuto emquanto processo sua solicitação!';
-      sendWhatsAppMessage(senderPhoneNumber, responseMessage);
-    } else {
-      const responseMessage = 'Desculpe, não entendi. Por favor, faça outra pergunta.';
-      sendWhatsAppMessage(senderPhoneNumber, responseMessage);
+    switch (objeto1.estado) {
+      case Estados.SAUDACAO:
+        responseMessage = 'Olá, tudo bem? Sou o assistente virtual. Como posso ajudar você hoje? \n'+
+                          'Temos as Seguintes opções: \n'+
+                          ' - Marcar horário \n'+
+                          ' - Cancelar horário \n'+
+                          ' - Histórico de horário';
+        break;
+      case Estados.MARCAR: 
+              responseMessage = 'Muito bem, me informe seu nome por favor.'; 
+              objeto1.estado = Estados.NOME;
+              break;
+      case Estados.DATA: 
+              responseMessage = 'Me informe a data pretendida.'; 
+              break;
+      case Estados.HORA: 
+              responseMessage = 'Me informe a hora pretendida.'; 
+              break;
+      default: responseMessage = 'Desculpe, não entendi. Por favor, faça outra pergunta.';        
     }
   
+    sendWhatsAppMessage(senderPhoneNumber, responseMessage);
     res.status(200).send('Status: Successful ')
 
 });
+
+
+// Enum para os estados
+const Estados = {
+  UNDEFINED: 'undefined',
+  SAUDACAO: 'Saudação',
+  MARCAR: 'Marcar',
+  CANCELAR: 'Cancelar',
+  DATA: 'Data',
+  HORA: 'Hora',
+  NOME: 'Nome',
+};
+
+
+// Objeto Singleton
+class Singleton {
+  constructor() {
+    this._nome = '';
+    this._estado = Estados.UNDEFINED; // Estado padrão
+  }
+
+  get nome() {
+    return this._nome;
+  }
+
+  set nome(novoNome) {
+    this._nome = novoNome;
+  }
+
+  get estado() {
+    return this._estado;
+  }
+
+  set estado(novoEstado) {
+    if (Object.values(Estados).includes(novoEstado)) {
+      this._estado = novoEstado;
+    } else {
+      console.error('Estado inválido');
+    }
+  }
+
+  imprimirMensagem() {
+    console.log(`Nome: ${this._nome}`);
+    console.log(`Estado: ${this._estado}`);
+  }
+
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new Singleton();
+    }
+    return this.instance;
+  }
+}
 
 // Função para enviar mensagens de volta para o WhatsApp
 function sendWhatsAppMessage(to, message) {
